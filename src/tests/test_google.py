01f7bfd6de0f876d79bd11638dfa0b2d4bf1b7a6
Andrey Kovalenko <kovalenko.andrey.odessa@gmail.com>
2019-06-17 21:57:57 +0300

    First Pytest for Goole

--- src/tests/test_google.py
+++ src/tests/test_google.py
@@ -13,6 +13,8 @@ class TestGoogleSearch:
 
     def test_google(self, driver_initialization):
         assert "Google" in driver_initialization.page_source
-        driver_initialization.find_element_by_xpath("//input[@name='q']")
-        driver_initialization.find_element_by_name("q")
+        input_field=driver_initialization.find_element_by_xpath("//input[@name='q']")
+        # driver_initialization.find_element_by_name("q")
+        input_field.send_keys("Pythonic")
+        input_field.send_keys(Keys.ENTER)
 # //*[@id="tsf"]/div[2]/div/div[1]/div/div[1]/input
