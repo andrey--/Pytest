0dbc6ef6997b6327054932b2ff0a95de0e066aac
Andrey Kovalenko <kovalenko.andrey.odessa@gmail.com>
2019-06-17 21:55:16 +0300

    First Pytest for Goole

--- src/tests/test_google.py
+++ src/tests/test_google.py
@@ -1,7 +1,18 @@
+import pytest
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
-def test_google():
-    driver=webdriver.Chrome()
-    driver.get("http://google.com")
-    assert "Google" in driver.page_source
-    driver.quit()
+
+class TestGoogleSearch:
+    @pytest.yield_fixture(scope="module")
+    def driver_initialization(self):
+        driver = webdriver.Chrome()
+        driver.get("http://google.com")
+        yield driver
+        driver.quit()
+
+    def test_google(self, driver_initialization):
+        assert "Google" in driver_initialization.page_source
+        driver_initialization.find_element_by_xpath("//input[@name='q']")
+        driver_initialization.find_element_by_name("q")
+# //*[@id="tsf"]/div[2]/div/div[1]/div/div[1]/input
